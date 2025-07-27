647e95ea2f5811ee6daa58b71153f85cbb5853dc
1p22geo <1p22geodecki@gmail.com>
2025-07-27 18:39:53 +0200

    patch: added UI if no more samples to load

--- slice_static/js/util.js
+++ slice_static/js/util.js
@@ -5,10 +5,10 @@ function isElementInViewport(el) {
     rect.top >= 0 &&
     rect.left >= 0 &&
     rect.bottom <=
-    (window.innerHeight ||
-      document.documentElement.clientHeight) /* or $(window).height() */ &&
+      (window.innerHeight ||
+        document.documentElement.clientHeight) /* or $(window).height() */ &&
     rect.right <=
-    (window.innerWidth ||
-      document.documentElement.clientWidth) /* or $(window).width() */
+      (window.innerWidth ||
+        document.documentElement.clientWidth) /* or $(window).width() */
   );
 }
